7f699b2b3b5963f29c23bf0c2e49d7c7374ee756
Kylta <Christophe.bugnon02@gmail.com>
2019-03-23 10:42:00 +0100

    Add ApiClient for RecipeDetail

---
+++ Reciplease/Reciplease/EntityGateway/API/Entities/ApiRecipeDetail.swift
@@ -0,0 +1,9 @@
+//
+//  ApiRecipeDetail.swift
+//  Reciplease
+//
+//  Created by Christophe Bugnon on 23/03/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import Foundation
