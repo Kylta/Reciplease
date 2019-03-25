800c448b90f54ec8ce72ec4495021b325e222ca3
Kylta <Christophe.bugnon02@gmail.com>
2019-03-25 03:49:16 +0100

    Put back end to end test

--- Reciplease/RecipleaseTests/ApiClientTest.swift
+++ Reciplease/RecipleaseTests/ApiClientTest.swift
@@ -9,19 +9,19 @@
 import XCTest
 @testable import Reciplease
 
-//class ApiClientTest: XCTestCase {
-//    func test_apiClient_giveRecipeOnGetRequest() {
-//        let apiClient = ApiClientImplementation()
-//        let url = URL(string: "https://api.yummly.com/v1/api/recipes?_app_id=82f4a536&_app_key=51bc109f3d02f621f3e62397cd754d62&q=POTATOES&requirePictures=true")!
-//        let apiGateway = ApiRecipesGatewayImplementation(url: url, client: apiClient)
-//
-//        apiGateway.fetchRecipes { response in
-//            switch response {
-//            case let .success(recipe):
-//                XCTAssertNotNil(recipe)
-//            case let .failure(error):
-//                XCTFail("Expected result but got \(error) instead")
-//            }
-//        }
-//    }
-//}
+class ApiClientTest: XCTestCase {
+    func test_apiClient_giveRecipeOnGetRequest() {
+        let apiClient = ApiClientImplementation()
+        let url = URL(string: "https://api.yummly.com/v1/api/recipes?_app_id=82f4a536&_app_key=51bc109f3d02f621f3e62397cd754d62&q=POTATOES&requirePictures=true")!
+        let apiGateway = ApiRecipesGatewayImplementation(url: url, client: apiClient)
+
+        apiGateway.fetchRecipes { response in
+            switch response {
+            case let .success(recipe):
+                XCTAssertNotNil(recipe)
+            case let .failure(error):
+                XCTFail("Expected result but got \(error) instead")
+            }
+        }
+    }
+}
